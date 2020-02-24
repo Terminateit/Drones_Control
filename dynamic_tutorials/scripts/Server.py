a0aecc321e1b15b013d280018a6dd2b197676d55
Ilia <ilya.terminator.27@gmail.com>
2020-02-24 20:23:54 +0300

    DynamicReconfigure add

---
+++ dynamic_tutorials/scripts/Server.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+import rospy
+
+from dynamic_reconfigure.server import Server
+from dynamic_tutorials.cfg import TutorialsConfig
+
+def callback(config, level):
+    rospy.loginfo("""Reconfigure Request: {Kp}, {Kd}""".format(**config))
+    return config
+
+if __name__ == "__main__":
+    rospy.init_node("dynamic_tutorials", anonymous = False)
+
+    srv = Server(TutorialsConfig, callback)
+    rospy.spin()
